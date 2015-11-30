8a838ed92ea7604829afd1af9f12fa9932eb7a5d
vic <soon530@gmail.com>
2015-11-30 16:10:01 +0800

    Add BeaconInfo to store each beacon url.

---
+++ app/src/main/java/tw/com/defood/beacon/BeaconInfo.java
@@ -0,0 +1,83 @@
+package tw.com.defood.beacon;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import org.altbeacon.beacon.Beacon;
+
+@ParseClassName("Beacon")
+public class BeaconInfo extends ParseObject {
+    private boolean mIsYoutubeOpen = true;
+    private boolean mIsWebsiteOpen = true;
+
+
+    private static ParseQuery<BeaconInfo> query;
+
+    public static ParseQuery<BeaconInfo> getQuery() {
+        return ParseQuery.getQuery(BeaconInfo.class);
+    }
+
+    public String getName() {
+        String value = getString("name");
+        return value;
+    }
+
+    public void setName(String value) {
+        put("name", value);
+    }
+
+    public String getMajor() {
+        String value = getString("major");
+        return value;
+    }
+
+    public void setMajor(String value) {
+        put("major", value);
+    }
+
+    public String getMinor() {
+        String value = getString("minor");
+        return value;
+    }
+
+    public void setMinor(String value) {
+        put("minor", value);
+    }
+
+    public String getType() {
+        String value = getString("type");
+        return value;
+    }
+
+    public void setType(String value) {
+        put("type", value);
+    }
+
+    public String getUrl() {
+        String value = getString("url");
+        return value;
+    }
+
+    public void setUrl(String value) {
+        put("url", value);
+    }
+
+    public boolean getIsYoutubeOpen() {
+        return mIsYoutubeOpen;
+    }
+
+    public void setIsYoutubeOpen(boolean value) {
+        mIsYoutubeOpen = value;
+    }
+
+    public boolean getIsWebsiteOpen() {
+        return mIsWebsiteOpen;
+    }
+
+    public void setIsWebsiteOpen(boolean value) {
+        mIsWebsiteOpen = value;
+    }
+
+
+}

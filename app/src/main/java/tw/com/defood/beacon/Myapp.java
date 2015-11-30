40379042453dc88070658da76f13df54d560cb00
vic <soon530@gmail.com>
2015-11-30 16:08:15 +0800

    Init Parse server with key.

--- app/src/main/java/tw/com/defood/beacon/Myapp.java
+++ app/src/main/java/tw/com/defood/beacon/Myapp.java
@@ -3,6 +3,9 @@ package tw.com.defood.beacon;
 import android.app.Application;
 import android.content.Intent;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Region;
@@ -17,11 +20,18 @@ public class Myapp extends Application implements BootstrapNotifier {
     private BackgroundPowerSaver backgroundPowerSaver;
     private boolean haveDetectedBeaconsSinceBoot = false;
 
+    private static final String APPLICATION_ID = "eErBLqF1Imny0NSMjqkZpVOxODv4ngAZdew7TktX";
+    private static final String CLIENT_KEY = "Z3GjMkBg7HreWEWniwouV5faNAFVHVtde3PUZXw5";
+
+
     @Override
     public void onCreate() {
         super.onCreate();
-        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
+        ParseObject.registerSubclass(BeaconInfo.class);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
+        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
         beaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
@@ -30,7 +40,9 @@ public class Myapp extends Application implements BootstrapNotifier {
         regionBootstrap = new RegionBootstrap(this, region);
 
         backgroundPowerSaver = new BackgroundPowerSaver(this);
+
     }
+
 
     @DebugLog
     @Override

61b4bfcb2e0e7effa8d801eff94a6189b3609d31
vic <soon530@gmail.com>
2015-12-02 13:13:33 +0800

    Add WebView to DetailActivity.

--- app/src/main/java/tw/com/defood/beacon/DetailActivity.java
+++ app/src/main/java/tw/com/defood/beacon/DetailActivity.java
@@ -2,6 +2,10 @@ package tw.com.defood.beacon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(Helper.getBeaconInfoFromCache().getUrl());
+
     }
 }
